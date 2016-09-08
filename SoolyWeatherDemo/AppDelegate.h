21cb9d4d21e5dcc87174daddc3e65f2c3c9870c3
SoolyChristina <sooly@soolychristinademacbook-air.local>
2016-09-08 15:18:13 +0800

    Initial Commit

---
+++ SoolyWeatherDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoolyWeatherDemo
+//
+//  Created by SoolyChristina on 16/9/8.
+//  Copyright © 2016年 SoolyChristina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoolyWeatherDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoolyWeatherDemo
+//
+//  Created by SoolyChristina on 16/9/8.
+//  Copyright © 2016年 SoolyChristina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
